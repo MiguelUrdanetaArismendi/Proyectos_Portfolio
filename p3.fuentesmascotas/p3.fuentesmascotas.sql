16615f85459e6129064ff322d81029917c992403
MiguelUrdanetaArismendi <miguelurdaneta346@gmail.com>
2024-10-29 22:23:32 +0100

    Add files via upload

---
+++ p3.fuentesmascotas/p3.fuentesmascotas.sql
@@ -0,0 +1,57 @@
+SELECT *
+FROM `p3.fuentesmascotas`;
+
+-- Standarize date format
+
+SELECT FECHA_INSTALACION, DATE (FECHA_INSTALACION)
+FROM `p3.fuentesmascotas`
+WHERE FECHA_INSTALACION <> '' AND FECHA_INSTALACION IS NOT NULL;
+
+ALTER TABLE `p3.fuentesmascotas`
+ADD fecha_instalacion_converted DATE;
+
+UPDATE `p3.fuentesmascotas`
+SET fecha_instalacion_converted = DATE (FECHA_INSTALACION)
+WHERE FECHA_INSTALACION <> '' AND FECHA_INSTALACION IS NOT NULL;
+
+-- Separate address
+
+SELECT
+    DIRECCION,
+    SUBSTRING_INDEX(DIRECCION, ' ', 1) AS direccion_tipo_de_via,
+    TRIM(SUBSTRING(SUBSTRING_INDEX(DIRECCION, ',', 1), LENGTH(SUBSTRING_INDEX(DIRECCION, ' ', 1)) + 2)) AS direccion_nombre,
+    TRIM(SUBSTRING_INDEX(DIRECCION, ',', -1)) AS direccion_numero
+FROM `p3.fuentesmascotas`;
+
+ALTER TABLE `p3.fuentesmascotas`
+ADD direccion_tipo_de_via VARCHAR(255);
+
+ALTER TABLE `p3.fuentesmascotas`
+ADD direccion_nombre VARCHAR(255);
+
+ALTER TABLE `p3.fuentesmascotas`
+ADD direccion_numero VARCHAR(255);
+
+UPDATE `p3.fuentesmascotas`
+SET direccion_tipo_de_via = SUBSTRING_INDEX(DIRECCION, ' ', 1);
+
+UPDATE `p3.fuentesmascotas`
+SET direccion_nombre = TRIM(SUBSTRING(SUBSTRING_INDEX(DIRECCION, ',', 1), LENGTH(SUBSTRING_INDEX(DIRECCION, ' ', 1)) + 2));
+
+UPDATE `p3.fuentesmascotas`
+SET direccion_numero = TRIM(SUBSTRING_INDEX(DIRECCION, ',', -1))
+
+SELECT USO,
+CASE WHEN 
+	USO LIKE '%personas%' THEN 'Sí'
+    ELSE 'No' END
+FROM `p3.fuentesmascotas`;
+
+ALTER TABLE `p3.fuentesmascotas`
+ADD apto_consumo_humano CHAR(2);
+
+UPDATE `p3.fuentesmascotas`
+SET apto_consumo_humano = 	
+	CASE WHEN 
+		USO LIKE '%personas%' THEN 'Sí'
+		ELSE 'No' END
